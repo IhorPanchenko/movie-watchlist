55a488eb88e6824959868fd9f7927ffbcf5ab66f
Ihor Panchenko <ipanchenko91@gmail.com>
2024-10-24 14:51:38 +0200

    Improve MovieDetailsModal component

--- src/features/movie/components/MovieDetailsModal.js
+++ src/features/movie/components/MovieDetailsModal.js
@@ -1,7 +1,23 @@
+import PropTypes from "prop-types";
 import { FaPlus, FaTrash, FaTimes } from "react-icons/fa";
 
 const MovieDetailsModal = ({
-  movie,
+  movie: {
+    Title,
+    Year,
+    Rated,
+    Runtime,
+    Metascore,
+    imdbRating,
+    Poster,
+    Genre,
+    Plot,
+    Director,
+    Writer,
+    Actors,
+    Country,
+    Awards,
+  },
   onClose,
   handleToggleWatchlist,
   isInWatchlist,
@@ -14,9 +30,12 @@ const MovieDetailsModal = ({
           onClick={handleToggleWatchlist}
           className={`absolute -top-4 right-7 flex items-center justify-center w-8 h-8 text-white rounded-full transition ${
             isInWatchlist
-              ? "bg-red-600 hover:bg-red-700 border border-red-400" // Red for removing from watchlist
-              : "bg-green-600 hover:bg-green-700 border border-green-400" // Green for adding to watchlist
-          }`} // Adding a shadow for depth
+              ? "bg-red-600 hover:bg-red-700 border border-red-400"
+              : "bg-green-600 hover:bg-green-700 border border-green-400"
+          }`}
+          aria-label={
+            isInWatchlist ? "Remove from watchlist" : "Add to watchlist"
+          }
         >
           {isInWatchlist ? (
             <FaTrash className="text-sm" />
@@ -36,15 +55,13 @@ const MovieDetailsModal = ({
         {/* Movie Info Header */}
         <div className="flex justify-between items-baseline mb-4">
           <div className="w-3/4">
-            <h2 className="text-2xl font-bold text-gray-900 mb-2">
-              {movie.Title}
-            </h2>
+            <h2 className="text-2xl font-bold text-gray-900 mb-2">{Title}</h2>
             <div className="flex text-gray-600">
-              <span>{movie.Year}</span>
+              <span>{Year}</span>
               <div className="mx-2 my-auto w-1 h-1 bg-gray-500 rounded-full"></div>
-              <span>{movie.Rated}</span>
+              <span>{Rated}</span>
               <div className="mx-2 my-auto w-1 h-1 bg-gray-500 rounded-full"></div>
-              <span>{movie.Runtime}</span>
+              <span>{Runtime}</span>
             </div>
           </div>
 
@@ -52,11 +69,11 @@ const MovieDetailsModal = ({
           <div className="w-1/4 flex items-center justify-between text-center text-gray-800">
             <div>
               <div className="text-base font-semibold">Metascore</div>
-              <div className="text-sm">{movie.Metascore}%</div>
+              <div className="text-sm">{Metascore}%</div>
             </div>
             <div>
               <div className="text-base font-semibold">IMDb</div>
-              <div className="text-sm">{movie.imdbRating}/10</div>
+              <div className="text-sm">{imdbRating}/10</div>
             </div>
           </div>
         </div>
@@ -67,8 +84,8 @@ const MovieDetailsModal = ({
           <div className="w-1/3 min-h-[360px]">
             <img
               className="w-full h-auto rounded-md shadow-md"
-              src={movie.Poster}
-              alt={`${movie.Title} poster`}
+              src={Poster}
+              alt={`${Title} poster`}
             />
           </div>
 
@@ -76,8 +93,8 @@ const MovieDetailsModal = ({
           <div className="w-2/3 pl-6 text-gray-700">
             {/* Genres */}
             <div className="flex flex-wrap gap-2 mb-4">
-              {movie.Genre
-                ? movie.Genre.split(", ").map((genre, index) => (
+              {Genre
+                ? Genre.split(", ").map((genre, index) => (
                     <span
                       key={index}
                       className="border border-gray-300 bg-gray-100 text-gray-700 px-2 py-1 rounded-full text-sm"
@@ -89,27 +106,49 @@ const MovieDetailsModal = ({
             </div>
 
             {/* Additional Info */}
-            <p className="text-base mb-3">{movie.Plot || "N/A"}</p>
+            <p className="text-base mb-3">{Plot || "N/A"}</p>
             <p className="text-base mb-2">
-              <strong>Director:</strong> {movie.Director || "N/A"}
+              <strong>Director:</strong> {Director || "N/A"}
             </p>
             <p className="text-base mb-2">
-              <strong>Writer:</strong> {movie.Writer || "N/A"}
+              <strong>Writer:</strong> {Writer || "N/A"}
             </p>
             <p className="text-base mb-2">
-              <strong>Cast:</strong> {movie.Actors || "N/A"}
+              <strong>Cast:</strong> {Actors || "N/A"}
             </p>
             <p className="text-base mb-2">
-              <strong>Country:</strong> {movie.Country || "N/A"}
+              <strong>Country:</strong> {Country || "N/A"}
             </p>
             <p className="text-base mb-2">
-              <strong>Awards:</strong> {movie.Awards || "N/A"}
+              <strong>Awards:</strong> {Awards || "N/A"}
             </p>
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+MovieDetailsModal.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.string,
+    Rated: PropTypes.string,
+    Runtime: PropTypes.string,
+    Metascore: PropTypes.string,
+    imdbRating: PropTypes.string,
+    Poster: PropTypes.string,
+    Genre: PropTypes.string,
+    Plot: PropTypes.string,
+    Director: PropTypes.string,
+    Writer: PropTypes.string,
+    Actors: PropTypes.string,
+    Country: PropTypes.string,
+    Awards: PropTypes.string,
+  }).isRequired,
+  onClose: PropTypes.func.isRequired,
+  handleToggleWatchlist: PropTypes.func.isRequired,
+  isInWatchlist: PropTypes.bool.isRequired,
 };
 
 export default MovieDetailsModal;
